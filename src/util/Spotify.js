1b7dc3b2393ab08e0e3e1e31fe84a3170788d834
Ulimativo <ulrich.raab@outlook.com>
2023-01-02 07:43:47 +0100

    finalized Spotify module

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -12,7 +12,7 @@ const Spotify = {
     const accesstokenMatch = window.location.href.match(/access_token=([^&]*)/);
     const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
 
-    if (accessTokenMatch && expiresInMatch) {
+    if (accesstokenMatch && expiresInMatch) {
       accessToken = accesstokenMatch[1];
       const expiresIn = Number(expiresInMatch[1]);
       // This clears the parameters, allowing us to grab a new access token when it expires.
@@ -46,6 +46,39 @@ const Spotify = {
           album: track.album.name,
           uri: track.uri,
         }));
+      });
+  },
+
+  savePlaylist(name, trackUris) {
+    if (!name || !trackUris.length) {
+      return;
+    }
+
+    const accessToken = Spotify.getAccessToken();
+    const headers = { Authorization: `Bearer ${accessToken}` };
+    let userId;
+
+    return fetch("https://api.spotify.com/v1/me", { headers: headers })
+      .then((response) => response.json())
+      .then((jsonResponse) => {
+        userId = jsonResponse.id;
+        return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
+          headers: headers,
+          method: "POST",
+          body: JSON.stringify({ name: name }),
+        })
+          .then((response) => response.json())
+          .then((jsonResponse) => {
+            const playlistId = jsonResponse.id;
+            return fetch(
+              `https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`,
+              {
+                header: headers,
+                method: "POST",
+                body: JSON.stringify({ uris: trackUris }),
+              }
+            );
+          });
       });
   },
 };
